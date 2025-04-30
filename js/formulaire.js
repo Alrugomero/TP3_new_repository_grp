b804c61a9d7d5d36d893ddca38f4a746647d5956
Alain <alainrugomero@gmail.com>
2025-04-30 16:06:57 -0400

    changement fichier

--- js/formulaire.js
+++ js/formulaire.js
@@ -4,7 +4,8 @@
 /* DÉBUT variables globales */ 
 // ##########################
 
-
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const descriptionPseudo = /^[a-zA-Z]{3,25}$/;
 
 /* FIN variables globales */ 
 // ##########################
@@ -17,15 +18,170 @@ function afficherChoixJeu()
 	// Ces fonctions sont le point d'entrée pour le jeu choisi par l'utilisateur et se trouvent respectivement 
 	// dans les fichiers js/jeu_memoire.js et js/quiz.js
 	// À la fin de cette fonction, on doit vider le <main> et afficher le jeu choisi par l'utilisateur
+
+	const main = document.querySelector('main');
+	main.innerHTML ="";
+
+	const h2 = document.createElement('h2');
+    h2.textContent = "Choisissez votre jeu";
+    main.appendChild(h2);
+
+
+	const divChoix = document.createElement('div');
+    divChoix.style.display = 'flex';
+    divChoix.style.justifyContent = 'center';
+    divChoix.style.gap = '40px';
+
+
+	//Choix
+
+	const divQuiz = document.createElement('div');
+    const imgQuiz = document.createElement('img');
+    imgQuiz.src = "Maquettes/Quiz.png"; 
+    imgQuiz.alt = 'Quiz';
+    imgQuiz.style.cursor = 'pointer';
+    imgQuiz.style.width = '200px';
+    imgQuiz.addEventListener('click', init_quiz); // Appelle ton quiz
+    const labelQuiz = document.createElement('p');
+    labelQuiz.textContent = "Quiz de connaissances";
+    labelQuiz.style.textAlign = 'center';
+    divQuiz.append(imgQuiz, labelQuiz);
+
+
+
+	// Jeu mémoire
+	const divMemoire = document.createElement('div');
+	const imgMemoire = document.createElement('img');
+	imgMemoire.src = "Maquettes/jeuMemoire.png"; // Assure-toi que cette image existe
+	imgMemoire.alt = 'Jeu de mémoire';
+	imgMemoire.style.cursor = 'pointer';
+	imgMemoire.style.width = '200px';
+	imgMemoire.addEventListener('click', init_jeu_memoire); // À définir dans jeu_memoire.js
+	const labelMemoire = document.createElement('p');
+	labelMemoire.textContent = "Jeu de mémoire";
+	labelMemoire.style.textAlign = 'center';
+	divMemoire.append(imgMemoire, labelMemoire);
+	
+	divChoix.append(divQuiz, divMemoire);
+	main.appendChild(divChoix);
 }
 
-function validerFormulaire()
+function validerPrenom()
 {
-	console.log('validerFormulaire() : à la fin de cette fonction, si tout est valide, on peut appeler afficherChoixJeu()');
+	const lePrenom = document.getElementById("prenom").value.trim();
+
+	if(lePrenom === "")
+	{
+		document.getElementById("prenomError").textContent = "Champ prénom est requis.";
+
+		return false;
+	}
+
+	return true;
+	
+}
+
+function validerNom()
+{
+	const leNom = document.getElementById("nom").value.trim();
+
+	if(leNom==="")
+	{
+		document.getElementById("nomError").textContent ="Le champ nom est requis";
+		return false;
+	}
+	return true;
+}
+
+function validerPseudo()
+{
+	const lePseudo = document.getElementById("pseudo").value.trim();
+
+	if(lePseudo ===""){
+		document.getElementById("pseudoError").textContent = "Le champ pseudo est requis";
+		return false;
+	}
+	else if(!descriptionPseudo.test(lePseudo))
+	{
+		document.getElementById("pseudoError").textContent = "Le pseudo doit contenir uniquement des lettres, entre 3 et 25 caractères."
+		return false;
+	}
+	else if(descriptionPseudo.test(lePseudo))
+	{
+		document.getElementById("pseudoError").textContent = "Parfait";
+		return true;
+	}
+}
+
+function validerCourriel()
+{
+	const leCourriel = document.getElementById("courriel").value.trim();
+
+	if(leCourriel==="")
+	{
+		document.getElementById("courrielError").textContent ="Champ courriel est requis";
+		return false;
+	}
+	else if(!emailRegex.test(leCourriel))
+	{
+		document.getElementById("courrielError").textContent ="Adresse email incorrecte";
+		return false;
+	}
+	else if (emailRegex.test(leCourriel))
+	{
+		document.getElementById("courrielError").textContent = "Parfait"
+		return true;
+	}
+
+}
+
+function validerConfirmation()
+{
+	const confirmation = document.getElementById("confirmCourriel").value.trim();
+	const leCourriel = document.getElementById("courriel").value.trim();
+
+	if(confirmation ==="")
+	{
+		document.getElementById("confirmCourrielError").textContent="Champ courriel de confirmation est requis"
+		return false;
+	}
+	else if(confirmation!==leCourriel)
+	{
+		document.getElementById("confirmCourrielError").textContent="Les adresses email ne correspondent pas"
+		return false;
+	}
+	else if(confirmation === leCourriel)
+	{
+		document.getElementById("confirmCourrielError").textContent= "Bien";
+		return true;
+	}
+}
+
+function validerFormulaire(e)
+{
+	e.preventDefault();
+
+	const prenomValide = validerPrenom();
+	const nomValide = validerNom();
+	const pseudoValide = validerPseudo();
+	const courrielValide = validerCourriel();
+	const confirmationValide = validerConfirmation();
+
+	
+	// console.log('validerFormulaire() : à la fin de cette fonction, si tout est valide, on peut appeler afficherChoixJeu()');
+
+	if(prenomValide && nomValide && pseudoValide && courrielValide && confirmationValide)
+	{
+		afficherChoixJeu();
+	}
+	
+
 }
 
 function gererBtnInvite()
 {
+	const pseudo = "invité"
+	
 	// Test direct au quiz
 	// init_quiz();
 
@@ -33,12 +189,22 @@ function gererBtnInvite()
 	// init_jeu_memoire();
 }
 
-
 function init_formulaire() {
 
 	// Simple bouton pour passer le formulaire et aller au jeu de mémoire directement
 	let btnInvite = document.getElementById("btnInvite");
 	btnInvite.addEventListener("click", gererBtnInvite, false);
+
+	// let btnSoumet = document.getElementById("btnSoumettre");
+	// btnSoumet.addEventListener('click',validerFormulaire);
+
+	document.getElementById("prenom").addEventListener('input',validerPrenom);
+	document.getElementById("nom").addEventListener('input',validerNom);
+	document.getElementById("pseudo").addEventListener('input',validerPseudo);
+	document.getElementById("courriel").addEventListener('input',validerCourriel);
+	document.getElementById("confirmCourriel").addEventListener('input',validerConfirmation);
+	document.getElementById("btnSoumettre").addEventListener('click',validerFormulaire)
+
 }
 
 	
--- js/quiz.js
+++ js/quiz.js
@@ -4,31 +4,80 @@
 // 
 
 // Variables globale qui contient les données du quiz
-// Il s'agit d'un tableau d'objets, chaque objet contient une question, un tableau de réponses et l'indice de la bonne réponse
-// Vous remplacer son contenu par votre propre quiz, vos questions et réponses, etc.
+// Il s'agit d'un tableau d'objets, chaque objet contient une question, un tableau de reponses et l'indice de la bonne choix
+// Vous remplacer son contenu par votre propre quiz, vos questions et reponses, etc.
 // Vous pouvez même modifier entièrement la structure de cette variable si vous le désirez
 let donnees = [
     {
-        question: "À quoi sert un aria-label?",
-        réponses: [
-            "Ajouter du contenu sur une balise pour aider les lecteurs d'écran",
-            "À rien",
-            "Je ne sais pas"
+        question: "Quelle est la capitale de l'Autralie?",
+        reponses: [
+            "Canberra",
+            "Sydeney",
+            "Melbourne"
         ],
-        réponse: 0
+        choix: 0
     },
     {
-        question: "HTML vient de :",
-        réponses: [
-            "Hyper Typo Meta Lol",
-            "Hypertext markup language",
-            "Je ne sais pas"
+        question: "Quelle est la capitale du Canada?",
+        reponses: [
+            "Toronto",
+            "Montréal",
+            "Ottawa"
         ],
-        réponse: 1
+        choix: 2
+    },
+    {
+        question: "Quelle est la capitale de la côte d'ivoire?",
+        reponses: [
+            "Bouaké",
+            "Abidjan",
+            "Yamoussoukro"
+        ],
+        choix: 1
+    },
+    {
+        question: "Quelle est la capitale de la France?",
+        reponses: [
+            "Paris",
+            "Marseille",
+            "Lyon"
+        ],
+        choix: 0
+    },
+    {
+        question: "Quelle est la capitale du Japon?",
+        reponses: [
+            "Osaka",
+            "Tokyo",
+            "Kyoto"
+        ],
+        choix: 1
+    },
+    {
+        question: "Quelle est la capitale du Mexique?",
+        reponses: [
+            "Guadalajara",
+            "Monterrey",
+            "Mexico"
+        ],
+        choix: 2
+    },
+    {
+        question: "Quelle est la capitale de l'allemagne?",
+        reponses: [
+            "Munich",
+            "Hambourg",
+            "Berlin"
+        ],
+        choix: 2
     }
+
 ];
 
+let iQuestion = 0;
+let score = 0;
 
+let index = 0;
 
 //* FIN variables globales */	
 // // ##########################
@@ -36,11 +85,149 @@ let donnees = [
 
 function init_quiz() {
 
-	// On doiter vider le <main> et on affiche le quiz, à partir de votre source de données "donnees", qui est une variable globale
-	// Vous remplacer son contenu par le contenu de votre quiz, vos questions et réponses, etc.
-	console.log("init_quiz() : initialisation du quiz");
-	console.log("donnees : ", donnees);
-	
+    // On doit  vider le <main> et on affiche le quiz, à partir de votre source de données "donnees", qui est une variable globale¸
+
+    const main = document.getElementById("main");
+    main.innerHTML = "";
+
+    afficherQuiz(iQuestion);
+
+
+    // Vous remplacer son contenu par le contenu de votre quiz, vos questions et reponses, etc.
+    console.log("init_quiz() : initialisation du quiz");
+    console.log("donnees : ", donnees);
+
+
+
+}
+
+function afficherQuiz(iQuestion) {
+
+    const place = document.getElementById("main");
+
+
+    let div = document.createElement("div");
+
+    let QuestionNbr = document.createElement("h2");
+
+    let h3 = document.createElement("h3");
+
+    let paragraphe = document.createElement("P");
+
+    let ol = document.createElement("ol");
+
+    let reponseText = document.createElement("input");
+
+    reponseText.type = 'text';
+
+    let suivant = document.createElement("button");
+    suivant.style.height = "60px";
+    suivant.style.width="120px";
+
+    suivant.addEventListener('click',boutton);
+
+    
+
+    boutton();
+
+
+    function boutton ()
+    {
+        if (index < donnees.length) {
+            const respo = donnees[index].reponses;
+    
+            console.log(respo);
+    
+            const quest = donnees[index].question;
+    
+            for (let i = 0; i < respo.length; i++) {
+    
+    
+                QuestionNbr.textContent = "Question " + (index + 1) + "/" + donnees.length;
+    
+                h3.textContent = quest;
+    
+    
+                paragraphe.textContent = "Veuillez saisir la bonne réponse";
+    
+    
+                let lesreponses = respo[i];
+    
+                let li = document.createElement("li");
+    
+                li.textContent = lesreponses;
+    
+                ol.appendChild(li);
+    
+                div.appendChild(QuestionNbr);
+                div.appendChild(h3);
+                div.appendChild(paragraphe);
+                div.appendChild(ol);
+                div.appendChild(reponseText);
+                div.appendChild(suivant);
+                place.appendChild(div);
+    
+    
+    
+            }
+    
+            deux();
+        }
+
+    }
+
+    function deux()
+    {
+        index++;
+        console.log(index)
+    }
+
+    //     let lesReponses = donnees[0].reponses;
+
+    //     let li = document.createElement("li");
+
+    //     li.textContent = lesReponses;
+
+    //     console.log(lesReponses);
+
+    //     ol.appendChild(li);
+    //     div.appendChild(QuestionNbr);
+    //     div.appendChild(h3);
+    //     div.appendChild(paragraphe);
+    //     div.appendChild(ol);
+    //     div.appendChild(reponseText);
+    //     div.appendChild(suivant);
+
+    //     place.appendChild(div);
+}
+
+// let index = 0;
+
+// function afficherReponses() {
+//     const container = document.getElementById("main");
+//     container.innerHTML = "";
+
+//     if (index < donnees.length) {
+//         const reponses = donnees[index].reponses;
+//         for (let i = 0; i < reponses.length; i++) {
+//             const div = document.createElement("li");
+//             div.textContent = reponses[i];
+//             container.appendChild(div);
+//         }
+//         index++;
+//     } else {
+//         container.textContent = "Fin du questionnaire.";
+//     }
+// }
+
+
+
+
+function traiterReponse() {
+
+}
+
+function afficherResultat() {
 
 }
 
